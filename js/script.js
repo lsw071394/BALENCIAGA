c2f59cd1b8b12a33510a3fc5fe62e28b3fb2f8e2
lsw071394 <sangwon0730@gmail.com>
2021-02-10 15:13:07 +0900

    .css

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
+$(function () {
+    // $('.arrco-btn').on('click', function () {
 
-$(".hover").mouseleave(
-    function () {
-      $(this).removeClass("hover");
+    //     $(this).parent().toggleClass('act')
+    // });
+
+
+    $('.hamburger').on('click', function () {
+        $(this).toggleClass('act');
+        $('nav').toggleClass('act');
+        navOn();
+    })
+    $('.pay-box > span:contains(+)').css({ 'color': '#3262FF' })
+
+    function navOn() {
+        if ($('.hamburger').hasClass('act')) {
+            $('.black_bg').css({ 'visibility': 'visible' })
+        } else {
+            $('.black_bg').css({ 'visibility': 'hidden' })
+        }
+
     }
-  );
-    
+})
