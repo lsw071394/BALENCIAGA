1697d306b280f8ffa0d3f048a58ec53e2588c0f2
lsw071394 <sangwon0730@gmail.com>
2021-02-15 14:24:27 +0900

    0215

--- js/script.js
+++ js/script.js
@@ -15,9 +15,143 @@ $(function () {
     function navOn() {
         if ($('.hamburger').hasClass('act')) {
             $('.black_bg').css({ 'visibility': 'visible' })
+            $('.white-wrap span').css({ 'background-color': '#F7F7F7' })
+            $('.white-wrap img').attr('src', '../img/icon_notice.svg')
         } else {
             $('.black_bg').css({ 'visibility': 'hidden' })
+            $('.white-wrap span').css({ 'background-color': '#262626' })
+            $('.white-wrap img').attr('src', '../img/icon_notice_black.svg')
         }
-
     }
+
+    var chart = new Highcharts.chart('container1', {
+
+        chart: {
+            type: 'variablepie',
+            // width: 200,
+            height: 220,
+            marginTop: 20
+        },
+        title: {
+            text: ''
+        },
+        credits: {
+            enabled: false
+        },
+        plotOptions: {
+            series: {
+                enableMouseTracking: false,
+            }
+        },
+
+        series: [{
+            innerSize: '30%',
+            zMin: 0,
+            // shadow: {
+            //     color: 'blue'
+            // },
+            data: [{
+                className: 'ds',
+                name: 'hello',
+                dataLabels: {
+                    enabled: false,
+                    borderRadius: (5, 0, 2, 5),
+                    backgroundColor: '#fff',
+                    padding: 8,
+                    x: -80,
+                    y: 20
+                },
+                y: 30,
+                z: 20,
+                color: '#32C5FF',
+            }, {
+                name: '',
+                y: 22,
+                z: 10,
+                color: '#02C04F'
+            }, {
+                name: '',
+                y: 19,
+                z: 10,
+                color: '#FFBC47'
+            }, {
+                name: '',
+                y: 10,
+                z: 10,
+                color: '#FF7315'
+            }, {
+                name: '',
+                y: 7,
+                z: 10,
+                color: '#DDDDDD'
+            },]
+        }],
+    });
+
+
+
+    Highcharts.chart('container', {
+        chart: {
+            type: 'line',
+            height: 220,
+
+        },
+        credits: {
+            enabled: false
+        },
+        title: {
+            text: ''
+        },
+
+        xAxis: {
+            categories: ['0' + 9 + '월', 10 + '월', 11 + '월', 12 + '월',
+            ],
+            labels: {
+                style: {
+                    fontSize: 16,
+                    color: '#262626',
+                    fontFamily: 'NanumSquare, sans-serif'
+                }
+            },
+        },
+        yAxis: {
+            title: {
+                text: ''
+            },
+            gridLineColor: '#F5F5F5'
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: false,
+                },
+                enableMouseTracking: false,
+                lineWidth: 1,
+            }
+        },
+        series: [{
+            name: '',
+            data: [10, 14, 13, 23.5],
+            marker: {
+                symbol: 'circle',
+                radius: 5
+            },
+            color: '#FA6400'
+        }, {
+            name: '',
+            data: [20, 25, 18, 20],
+            marker: { symbol: 'circle', radius: 5 },
+            color: '#32C5FF'
+        }],
+        navigation: {
+            buttonOptions: {
+                enabled: false
+            }
+        }
+    });
+    $('.highcharts-yaxis-labels text').remove();
+    $('.highcharts-legend').remove();
+    $('.highcharts-xaxis').remove();
+    $('.highcharts-yaxis-grid .highcharts-grid-line').eq(0).remove();
+    // $('.highcharts-grid-line').remove();
 })
