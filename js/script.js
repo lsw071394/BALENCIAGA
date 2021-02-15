b02f63575f9fa5ff97195b5bc2ce0226fbff509e
lsw071394 <sangwon0730@gmail.com>
2021-02-15 14:49:09 +0900

    sd

--- js/script.js
+++ js/script.js
@@ -16,11 +16,11 @@ $(function () {
         if ($('.hamburger').hasClass('act')) {
             $('.black_bg').css({ 'visibility': 'visible' })
             $('.white-wrap span').css({ 'background-color': '#F7F7F7' })
-            $('.white-wrap img').attr('src', '../img/icon_notice.svg')
+            $('.white-wrap .notice').addClass('on');
         } else {
             $('.black_bg').css({ 'visibility': 'hidden' })
             $('.white-wrap span').css({ 'background-color': '#262626' })
-            $('.white-wrap img').attr('src', '../img/icon_notice_black.svg')
+            $('.white-wrap .notice').removeClass('on');
         }
     }
 
@@ -148,10 +148,11 @@ $(function () {
                 enabled: false
             }
         }
+
     });
     $('.highcharts-yaxis-labels text').remove();
     $('.highcharts-legend').remove();
     $('.highcharts-xaxis').remove();
     $('.highcharts-yaxis-grid .highcharts-grid-line').eq(0).remove();
-    // $('.highcharts-grid-line').remove();
+
 })
