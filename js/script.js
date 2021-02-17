ace8090cdcec3b9d5d06ddda766b12c06e7338ae
lsw071394 <sangwon0730@gmail.com>
2021-02-17 16:25:27 +0900

    css

--- js/script.js
+++ js/script.js
@@ -5,37 +5,50 @@ $(function () {
     // });
 
 
-    $('.hamburger').on('click', function () {
-        $(this).toggleClass('act');
-        $('nav').toggleClass('act');
+
+    $('.header-wrap .hamburger').on('click', function () {
+        $(this).addClass('act');
+        $('nav').addClass('act');
         navOn();
     })
+
+    $('nav .hamburger').on('click', function () {
+        $('.header-wrap .hamburger').removeClass('act');
+        $('nav').removeClass('act');
+        navOn();
+    })
+
+
+
     $('.pay-box > span:contains(+)').css({ 'color': '#3262FF' })
 
     $('.black_bg').on('click', function () {
-        $('.hamburger').removeClass('act')
+        $('.header-wrap .hamburger').removeClass('act');
         $('nav').removeClass('act');
         navOn();
     })
 
     function navOn() {
-        if ($('.hamburger').hasClass('act')) {
+        if ($('.header-wrap .hamburger').hasClass('act')) {
             $('.black_bg').css({ 'visibility': 'visible' })
-            $('.white-wrap span').css({ 'background-color': '#F7F7F7' })
-            $('.white-wrap .notice').addClass('on');
             $('body').css({ 'overflow': 'hidden' })
         } else {
             $('.black_bg').css({ 'visibility': 'hidden' })
-            $('.white-wrap span').css({ 'background-color': '#262626' })
-            $('.white-wrap .notice').removeClass('on');
             $('body').css({ 'overflow': 'auto' })
         }
     }
 
+    $('.selected').on('click', function () {
+        stopPropagation();
+    });
+
+
+
     $('.challenge-item').on('click', function () {
         console.log('click')
         $(this).toggleClass('select');
-    })
+    });
+
     $('.challenge-result-text span').on('click', function () {
         if ($('#cb1').is(':checked') == true) {
             $('#cb1').prop('checked', false);
@@ -44,10 +57,9 @@ $(function () {
         }
     })
 
-    $('.highcharts-yaxis-labels text').remove();
-    $('.highcharts-legend').remove();
-    $('.highcharts-xaxis').remove();
-    $('.highcharts-yaxis-grid .highcharts-grid-line').eq(0).remove();
+
+
+
 
 
 
